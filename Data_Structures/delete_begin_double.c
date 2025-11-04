6f7099b7a248d788c6d8c8dd19b9130eb5b75832
Dasari Komal <komaldasarirao8499@gmail.com>
2025-11-05 00:56:59 +0530

    delete at begin double linkrd

---
+++ Data_Structures/delete_begin_double.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+struct Node *head = NULL, *temp = NULL, *temp1 = NULL , *temp2 = NULL;
+void deleteatbeginning()
+{
+    if (head == NULL)
+    {
+        printf("List is empty, nothing to delete!\n");
+        return;
+    }
+    else
+    {
+        temp2 = head;
+        head = head->next;
+        head->prev = NULL;
+        free(temp2);
+    }
+}
+void insert()
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    if (newnode == NULL)
+    {
+        printf("Memory not allocated!\n");
+        return;
+    }
+    printf("Enter data: ");
+    scanf("%d", &newnode->data);
+    newnode->prev = NULL;
+    newnode->next = NULL;
+    if (head == NULL)
+    {
+        head = temp = newnode;
+    }
+    else
+    {
+        temp->next = newnode;
+        newnode->prev = temp;
+        temp = newnode;
+    }
+}
+void traverse()
+{
+    temp1 = head;
+    while (temp1 != NULL)
+    {
+        printf("%d\t", temp1->data);
+        temp1 = temp1->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int choice;
+    do
+    {
+        printf("\n1.Insert\n2.traverse\n3.deleteatbeginning\n4.Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            traverse();
+            break;
+        case 3:
+            deleteatbeginning();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid Choice!");
+            break;
+        }
+    } while (choice != 5);
+}
