d74e79ae4da0c1afe82549accdb8bc4bac737a7b
Dasari Komal <komaldasarirao8499@gmail.com>
2025-11-05 18:41:04 +0530

    insert at circular at end

---
+++ Data_Structures/insert_end_circular.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *head = NULL;
+struct Node *tail = NULL;
+struct Node *temp = NULL;
+struct Node *newNode = NULL;
+void insert()
+{
+    newNode = malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
+        printf("Memory allocation failed!");
+        return;
+    }
+    printf("\nEnter the value to be inserted: ");
+    scanf("%d", &(newNode->data));
+    newNode->next = NULL;
+    temp = head;
+    if (tail == NULL)
+    {
+        tail = newNode;
+        tail->next = newNode;
+    }
+    else
+    {
+        newNode->next = tail->next;
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+void display()
+{
+    if (tail == NULL)
+    {
+        printf("\nThe list is empty!");
+        return;
+    }
+    temp = tail->next; // head
+    printf("\nThe elements of the linked list are:\n");
+    do
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != tail->next); 
+
+    printf("\nHead = %d", tail->next->data);
+    printf("\nTail = %d", tail->data);
+    printf("\nTail->next = %d (Head)", tail->next->data);
+    printf("\n");
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        printf("\n1. Insert at end\n2. Display\n3. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid choice! Please try again.");
+            break;
+        }
+    } while (choice != 4);
+    return 0;
+}
