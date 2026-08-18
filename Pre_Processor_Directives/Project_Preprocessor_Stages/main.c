4de57d13678576163a633fc6b19f8f69e409435a
DasariKomal9985 <komaldasarirao8499@gmail.com>
2026-08-18 13:21:38 +0530

    Pre processor stages

---
+++ Pre_Processor_Directives/Project_Preprocessor_Stages/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math.h"
+#define Number1 10
+#define Number2 20
+int main(void)
+{
+	int result = add(Number1, Number2);
+	printf("Result =%d\n",result);
+	return 0;
+}
